714b205a4d6266017acb1f33178ab9a99d6560d9
nawab0018 <135320636+nawab0018@users.noreply.github.com>
2024-06-07 11:02:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
-STREAM_API = (environ.get('STREAM_API', '0f5fa53392cbc06ec2a3b910a8b9458fad3271bc'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'zipshort.net'))
+STREAM_API = (environ.get('STREAM_API', '6484c1a6c73a7f077ae8687d7b6cf4f5f7ce565a'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/nawabbotupdate/15'))
 
 # Admins, Channels & Users
@@ -59,8 +59,8 @@ VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( F
 HOWTOVERIFY = environ.get('HOWTOVERIFY','https://t.me/nawabbotupdate/15') # How to open tutorial link for verification
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'ziplinker.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0f5fa53392cbc06ec2a3b910a8b9458fad3271bc')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'zipshort.net')
+SHORTLINK_API = environ.get('SHORTLINK_API', '6484c1a6c73a7f077ae8687d7b6cf4f5f7ce565a')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
